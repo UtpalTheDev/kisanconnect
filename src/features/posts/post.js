ba3fe65e6650307c029d8ecafb9f349599941df0
Utpal Pati <74959712+UtpalTheDev@users.noreply.github.com>
2021-06-12 14:50:11 +0530

    feat: post add, comment add, like add

--- src/features/posts/post.js
+++ src/features/posts/post.js
@@ -1,32 +1,157 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchPosts, likeButtonPressed } from "./postslice";
+import {
+  fetchPosts,
+  likeButtonPressed,
+  commentSendButtonPressed,
+  commentButtonPressed,
+  postButtonPressed
+} from "./postslice";
 
 export default function Posts() {
-  let { postStatus, postError, postData } = useSelector((state) => state.post);
-  // let error = useSelector((state) => state.post.error);
+  const [post, setPost] = useState("");
+  const [modal, setModal] = useState("");
+  let { getPostStatus, postError, postData } = useSelector(
+    (state) => state.post
+  );
   let dispatch = useDispatch();
-  // console.log(postStatus,postError,postData);
+  console.log(getPostStatus, postError, postData);
+  const userID = "utpal8";
   useEffect(() => {
-    if (postStatus === "idle") {
-      dispatch(fetchPosts);
+    if (getPostStatus === "idle") {
+      dispatch(fetchPosts());
     }
-  }, [postStatus, dispatch]);
+  }, [getPostStatus, dispatch]);
   return (
     <>
+      <div>
+        <input
+          value={post}
+          onChange={(e) => {
+            setPost(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              postButtonPressed({
+                postobj: {
+                  caption: post,
+                  likes: [],
+                  user: {
+                    userID: userID,
+                    name: "up"
+                  }
+                }
+              })
+            );
+          }}
+        >
+          Post
+        </button>
+      </div>
       <div>
         {postData.map((item) => {
           return (
             <div>
+              <div>{`"${item.user.name}"`}</div>
               <div>{item.caption}</div>
-              <button onClick={() => dispatch(likeButtonPressed(item.postID))}>
-                {item.likes} like
+              <button
+                onClick={() =>
+                  dispatch(likeButtonPressed({ postID: item._id, userID }))
+                }
+              >
+                {item.likes.length} like
               </button>
+              <button
+                onClick={() => {
+                  dispatch(commentButtonPressed(item._id));
+                  setModal(item._id);
+                }}
+              >
+                Comment
+              </button>
+              {modal === item._id && (
+                <Comment postID={item._id} setModal={setModal} postObj={item} />
+              )}
+
               <hr />
             </div>
           );
         })}
       </div>
     </>
+  );
+}
+
+function Comment({ postID, setModal, postObj }) {
+  const [comment, setComment] = useState("");
+  let { commentData } = useSelector((state) => state.post);
+  let dispatch = useDispatch();
+  console.log("commentData", commentData);
+  const userID = "utpal8";
+  return (
+    <div className="comment">
+      <div className="comment-header">
+        Comments
+        <button
+          className="comment-header-backbutton"
+          onClick={() => {
+            setModal("");
+          }}
+        >
+          back
+        </button>
+      </div>
+      <div className="comment-onpost">
+        <div>{postObj.user.name}</div>
+        <div>{postObj.caption}</div>
+      </div>
+      <hr />
+      <div className="comment-feed">
+        <div>
+          {commentData
+            .filter((commentobj) => commentobj.postID === postID)
+            .map((filterdata) => {
+              return (
+                <div>
+                  <div>{filterdata.user.name}</div>
+                  <div>{filterdata.caption}</div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="comment-input">
+        <input
+          value={comment}
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+
+        <button
+          onClick={() => {
+            dispatch(
+              commentSendButtonPressed({
+                commentobj: {
+                  postID: postID,
+                  caption: comment,
+                  likes: [],
+                  reply: [],
+                  user: {
+                    userID: userID,
+                    name: "up"
+                  }
+                }
+              })
+            );
+            setComment("");
+          }}
+        >
+          Comment
+        </button>
+      </div>
+    </div>
   );
 }
--- src/features/posts/postslice.js
+++ src/features/posts/postslice.js
@@ -2,49 +2,138 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchPosts = createAsyncThunk("post/fetchposts", async () => {
-  const response = await axios.get("");
+  const response = await axios.get(
+    "https://social-media-demo.utpalpati.repl.co/posts"
+  );
+  // console.log("l",response);
   return response.data;
 });
+export const likeButtonPressed = createAsyncThunk(
+  "post/likeButtonPressed",
+  async (likeupdateobj) => {
+    const response = await axios.post(
+      "https://social-media-demo.utpalpati.repl.co/posts/likes",
+      likeupdateobj
+    );
+    //  console.log("l",response);
+    return response.data;
+  }
+);
+export const commentSendButtonPressed = createAsyncThunk(
+  "post/commentSendButtonPressed",
+  async (commentobj) => {
+    const response = await axios.post(
+      "https://social-media-demo.utpalpati.repl.co/posts/comment",
+      commentobj
+    );
+    //  console.log("l",response);
+    return response.data;
+  }
+);
+export const commentButtonPressed = createAsyncThunk(
+  "post/commentButtonPressed",
+  async (postID) => {
+    const response = await axios.get(
+      `https://social-media-demo.utpalpati.repl.co/posts/comment/${postID}`
+    );
+    //  console.log("l",response);
+    return response.data;
+  }
+);
+export const postButtonPressed = createAsyncThunk(
+  "post/postButtonPressed",
+  async (postobj) => {
+    const response = await axios.post(
+      "https://social-media-demo.utpalpati.repl.co/posts",
+      postobj
+    );
+    //  console.log("l",response);
+    return response.data;
+  }
+);
 
 export const postslice = createSlice({
   name: "post",
   initialState: {
-    postStatus: "idle",
+    getPostStatus: "idle",
+    sendPostStatus: "idle",
+    commentPostStatus: "idle",
+    commentGetStatus: "idle",
     postError: null,
-    postData: [
-      {
-        postID: "p1201",
-        caption: "learning redux",
-        likes: 22,
-        user: {
-          userID: "u1234",
-          name: "tanay"
-        }
-      }
-    ]
+    commentData: [],
+    postData: []
   },
-  reducers: {
-    likeButtonPressed: (state, action) => {
-      let finddata = state.postData.find(
-        (item) => item.postID === action.payload
-      );
-      finddata.likes = finddata.likes + 1;
-    }
-  },
+  reducers: {},
   extraReducers: {
+    /*---------fetchpost-----------------------------*/
     [fetchPosts.pending]: (state, action) => {
-      state.status = "loading";
+      state.getPostStatus = "loading";
     },
     [fetchPosts.fulfilled]: (state, action) => {
-      state.status = "succeeded";
-      // state.data = action.payload;
+      state.getPostStatus = "succeeded";
+      state.postData = action.payload;
     },
     [fetchPosts.rejected]: (state, action) => {
-      state.status = "failed";
-      state.error = action.error.message;
+      state.getPostStatus = "failed";
+      state.postError = action.error.message;
+    },
+    /*---------likebutton-----------------------------*/
+    [likeButtonPressed.pending]: (state, action) => {
+      state.postStatus = "loading";
+    },
+    [likeButtonPressed.fulfilled]: (state, action) => {
+      state.postStatus = "succeeded";
+      const { _id, likes } = action.payload.post;
+      console.log(_id, likes);
+      state.postData.find((item) => item._id === _id).likes = likes;
+    },
+    [likeButtonPressed.rejected]: (state, action) => {
+      state.postStatus = "failed";
+      state.postError = action.error.message;
+    },
+    /*---------commentsendbutton-----------------------------*/
+    [commentSendButtonPressed.pending]: (state, action) => {
+      state.commentPostStatus = "loading";
+    },
+    [commentSendButtonPressed.fulfilled]: (state, action) => {
+      state.commentPostStatus = "succeeded";
+      const { comment } = action.payload;
+      state.commentData.push(comment);
+    },
+    [commentSendButtonPressed.rejected]: (state, action) => {
+      state.commentPostStatus = "failed";
+      state.postError = action.error.message;
+    },
+    /*---------commentbutton-----------------------------*/
+    [commentButtonPressed.pending]: (state, action) => {
+      state.commentGetStatus = "loading";
+    },
+    [commentButtonPressed.fulfilled]: (state, action) => {
+      state.commentGetStatus = "succeeded";
+      const { comment } = action.payload;
+      state.commentData = state.commentData.filter(
+        (item) => item.postID !== comment[0].postID
+      );
+      state.commentData.push(...comment);
+    },
+    [commentButtonPressed.rejected]: (state, action) => {
+      state.commentGetStatus = "failed";
+      state.postError = action.error.message;
+    },
+    /*---------postbutton-----------------------------*/
+    [postButtonPressed.pending]: (state, action) => {
+      state.sendPostStatus = "loading";
+    },
+    [postButtonPressed.fulfilled]: (state, action) => {
+      state.sendPostStatus = "succeeded";
+      const { post } = action.payload;
+      state.postData.push(post);
+    },
+    [postButtonPressed.rejected]: (state, action) => {
+      state.sendPostStatus = "failed";
+      state.postError = action.error.message;
     }
   }
 });
 
-export const { likeButtonPressed } = postslice.actions;
 export default postslice.reducer;
