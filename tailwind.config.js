0c59a34175ba92ddf99a78d9595e69dcadb03576
Utpal Pati <utpalpati8@gmail.com>
2021-06-25 20:31:22 +0000

    refactor: tailwind minimum height configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   theme: {
     extend: {
     },
+    minHeight: {
+      '0': '0',
+      '1':'1rem',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+     }
   },
   variants: {
     extend: {},
