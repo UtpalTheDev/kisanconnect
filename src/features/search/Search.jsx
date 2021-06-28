afe432e4882bbe64cafbd796f1cecfadfaaffafd
Utpal Pati <utpalpati8@gmail.com>
2021-06-28 08:18:20 +0000

    refactor: style add, reduce delay for debounce

--- src/features/search/Search.jsx
+++ src/features/search/Search.jsx
@@ -22,14 +22,15 @@ export default function Search() {
   };
   const debounceCaller = debounce((userName) => {
     dispatch(userSearching(userName));
-  }, 1000);
+  }, 300);
 
   useEffect(() => {
     return dispatch(clearMatchList());
   }, []);
   return (
-    <>
+    <div className="px-1 py-2">
       <input
+      className="w-full border rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:border-transparent"
         placeholder="search"
         onChange={(e) => {
           //setUserName(e.target.value);
@@ -38,22 +39,24 @@ export default function Search() {
         }}
       />
       {matchlist.length > 0 && (
-        <div>
+        <div className="pt-4 px-1">
           {matchlist.map((matchuser) => {
             return (
-              <>
+              <div className="py-2">
                 {matchuser._id !== userId ? (
-                  <Link to={`/${matchuser.userName}`}>
-                    <div>{matchuser.userName}</div>
+                  <Link to={`/${matchuser.userName}`} className="block flex items-center">
+                    <img src="https://images.unsplash.com/photo-1598601193393-e750daeefbca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ2fHxwZW9wbGV8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="w-10 h-10 rounded-full" />
+                    <div className="px-2">{matchuser.userName}</div>
                   </Link>
                 ) : (
-                  <Link to="/user">{matchuser.userName}</Link>
+                  <Link to="/user" className="block flex items-center">                   <img src="https://images.unsplash.com/photo-1598601193393-e750daeefbca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ2fHxwZW9wbGV8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="w-10 h-10 rounded-full" />
+                  <div className="px-2">{matchuser.userName}</div></Link>
                 )}
-              </>
+              </div>
             );
           })}
         </div>
       )}
-    </>
+    </div>
   );
 }
