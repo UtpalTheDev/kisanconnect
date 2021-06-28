f0dde419a02700d8113fe81f47c49cd6321aff04
Utpal Pati <utpalpati8@gmail.com>
2021-06-28 08:16:43 +0000

    refactor: postcomponent add

---
+++ src/features/posts/PostComponent.jsx
@@ -0,0 +1,93 @@
+import {useState} from "react"
+import Like from "./Like";
+import Comment from "./Comment";
+import TimeAgo from "react-timeago"
+import {useSelector,useDispatch} from "react-redux"
+import {Link} from "react-router-dom"
+import {
+  likeButtonPressed,
+  commentButtonPressed,
+  likeDataOnTextPressed,
+  postDeleteButtonPressed
+} from "./postslice";
+export default function PostComponent({eachpost}){
+  const [commentModal, setCommentModal] = useState("");
+  const [likeModal, setLikeModal] = useState("");
+  let { userName, userId, following } = useSelector((state) => state.user);
+  const dispatch=useDispatch()
+    return(
+        <div className="relative px-2 py-2 my-3 rounded-lg shadow-md bg-white">
+        <Link to={userName===eachpost.user.userName? `/user`:`/${eachpost.user.userName}`} className=" blockflex items-center">
+          <img className=" inline w-8 h-8 border-solid border-gray-300 rounded-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
+          <span className="p-1 ">{`${eachpost.user.userName}`}</span>
+        </Link>
+        <div className="py-2">{eachpost.caption}</div>
+        <div className="absolute right-1 top-1 text-xs text-gray-300 font-normal">
+          <TimeAgo date={eachpost.date} />
+        </div>
+        <div className="flex w-full">
+        <button
+          className="flex items-center"
+          onClick={() =>
+            dispatch(
+              likeButtonPressed({ postID: eachpost._id, userName: userName })
+            )
+          }
+        >
+          <span class="material-icons-outlined">
+          favorite
+          </span>
+        </button>
+        <span
+        className="text-sm px-1"
+          onClick={() => {
+            setLikeModal(eachpost._id);
+            dispatch(likeDataOnTextPressed(eachpost._id));
+          }}
+        >
+          {eachpost.likes.length} Likes
+        </span>
+        <button
+          className=" px-2 flex items-center"
+          onClick={() => {
+            dispatch(commentButtonPressed(eachpost._id));
+            setCommentModal(eachpost._id);
+          }}
+        >
+          <span class="material-icons-outlined text-blue-300">
+           textsms
+          </span>
+        </button>
+        {eachpost.user.userID === userId && (
+          <button
+            className="ml-auto flex items-center"
+            onClick={() => {
+              dispatch(postDeleteButtonPressed({ postdeleteobj: eachpost }));
+            }}
+          >
+            <span class="material-icons-outlined">
+            delete
+            </span>
+          </button>
+        )}
+        </div>  
+        
+        {commentModal === eachpost._id && (
+          <Comment
+            postID={eachpost._id}
+            setCommentModal={setCommentModal}
+            postObj={eachpost}
+          />
+        )}
+        {likeModal === eachpost._id && (
+          <Like
+            postID={eachpost._id}
+            setLikeModal={setLikeModal}
+            postObj={eachpost}
+          />
+        )}
+
+        
+      </div>
+    )
+}
--- src/features/posts/post.jsx
+++ src/features/posts/post.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {FaRegCommentDots} from "react-icons/fa"
+import PostComponent from "./PostComponent";
 import {
   fetchPosts,
   likeButtonPressed,
@@ -12,7 +12,6 @@ import {
   postDeleteButtonPressed
 } from "./postslice";
 import Search from "../search/Search";
-import TimeAgo from "react-timeago";
 import Like from "./Like";
 import Comment from "./Comment";
 export default function Posts() {
@@ -45,7 +44,7 @@ export default function Posts() {
           value={post}
           rows=""
           cols=""
-          className=" block w-full min-h-1 border-b-2 resize-none focus:outline-none
+          className=" block w-full min-h-1 border-b resize-none focus:outline-none
            focus:border-blue-300 mx-auto "
           placeholder="whats going on?"
           onChange={(e) => {
@@ -77,79 +76,7 @@ export default function Posts() {
       <div className="px-2 py-4 flex flex-col h-full ">
         {postData.map((item) => {
           return (
-            <div className="relative px-2 py-2 my-3 rounded-lg shadow-md bg-white">
-              <div className="flex items-center">
-                <img className=" inline w-8 h-8 border-solid border-gray-300 rounded-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
-                <span className="p-1 ">{`${item.user.userName}`}</span>
-              </div>
-              <div className="py-2">{item.caption}</div>
-              <div className="absolute right-1 top-1 text-xs text-gray-300 font-normal">
-                <TimeAgo date={item.date} />
-              </div>
-              <div className="flex w-full">
-              <button
-                className="flex items-center"
-                onClick={() =>
-                  dispatch(
-                    likeButtonPressed({ postID: item._id, userName: userName })
-                  )
-                }
-              >
-                <span class="material-icons-outlined">
-                favorite
-                </span>
-              </button>
-              <span
-              className="text-sm px-1"
-                onClick={() => {
-                  setLikeModal(item._id);
-                  dispatch(likeDataOnTextPressed(item._id));
-                }}
-              >
-                {item.likes.length} Likes
-              </span>
-              <button
-                className=" px-2 flex items-center"
-                onClick={() => {
-                  dispatch(commentButtonPressed(item._id));
-                  setCommentModal(item._id);
-                }}
-              >
-                <span class="material-icons-outlined text-blue-300">
-                 textsms
-                </span>
-              </button>
-              {item.user.userID === userId && (
-                <button
-                  className="ml-auto flex items-center"
-                  onClick={() => {
-                    dispatch(postDeleteButtonPressed({ postdeleteobj: item }));
-                  }}
-                >
-                  <span class="material-icons-outlined">
-                  delete
-                  </span>
-                </button>
-              )}
-              </div>  
-              
-              {commentModal === item._id && (
-                <Comment
-                  postID={item._id}
-                  setCommentModal={setCommentModal}
-                  postObj={item}
-                />
-              )}
-              {likeModal === item._id && (
-                <Like
-                  postID={item._id}
-                  setLikeModal={setLikeModal}
-                  postObj={item}
-                />
-              )}
-
-              
-            </div>
+              <PostComponent eachpost={item} />
           );
         })}
       </div>
