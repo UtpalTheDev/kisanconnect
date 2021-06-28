1a75b7c855722253f92921d53254d4b2988278d6
Utpal Pati <utpalpati8@gmail.com>
2021-06-28 08:26:11 +0000

    refactor: postcomponent add

--- src/features/user/User.jsx
+++ src/features/user/User.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-// import { useLogin } from "./LoginContext";
-// import { useReduce } from "./Reducer-context";
 import {BsFillLockFill} from "react-icons/bs"
 import { useSelector, useDispatch } from "react-redux";
 import { logOut } from "../auth/authSlice";
@@ -23,6 +21,7 @@ import {
   likeDataOnTextPressed,
   postDeleteButtonPressed
 } from "../posts/postslice";
+import PostComponent from "../posts/PostComponent";
 
 export default function User() {
   const [commentModal, setCommentModal] = useState("");
@@ -31,6 +30,7 @@ export default function User() {
   const {
     userName,
     name,
+    bio,
     userId,
     email,
     userposts,
@@ -54,7 +54,7 @@ export default function User() {
 
   return (
     <div className="user relative min-h-screen">
-      <div className="user-data px-2 py-2 bg-white">
+      <div className="user-data px-2 pt-4 pb-2 bg-white">
         <div className="flex items-center"><BsFillLockFill className="text-sm"/>
         <span className="text-lg">{userName}</span>
         </div>
@@ -70,8 +70,9 @@ export default function User() {
            </div>
            </div>
 
-        <div>{name}</div>
-        <div className="py-3">  
+        <div className="px-1">{name}</div>
+        <div className="px-1">{bio}</div>
+        <div className="py-3 px-1">  
         <Link to="/user/edit" className="block w-full border border-solid flex justify-center border-gray-200 ">Edit</Link>
         </div> 
       </div>
@@ -92,18 +93,21 @@ export default function User() {
       {followSuggestionList !== null &&
         followSuggestionList.map((item) => {
           return (
+            <Link to={`/${item.userName}`} className="block">
             <div className="w-28 flex-shrink-0 h-40 mx-2 flex-col text-center rounded-md border border-gray-300 box-border">
               <div className="rounded-b-md flex-grow">
                 <img className="w-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
               </div>
               <div>{item.userName}</div>
+              
 
               {followrequestSent.find(
                 (pendingobj) => pendingobj._id === item._id
               ) && (
                 <button
-                  className="text-black w-10/12 border-solid border border-gray-400 bg-white"
-                  onClick={() => {
+                  className="text-black w-10/12 border-solid border border-gray-400 bg-white text-sm"
+                  onClick={(e) => {
+                    e.preventDefault()
                     dispatch(followButtonPress({ followerId: item._id }));
                   }}
                 >
@@ -118,8 +122,9 @@ export default function User() {
                   (followingobj) => followingobj._id === item._id
                 ) && (
                   <button
-                  className="text-white bg-blue-700 w-10/12"
-                    onClick={() => {
+                  className="text-white border border-blue-800 bg-blue-800 w-10/12 text-sm"
+                    onClick={(e) => {
+                      e.preventDefault()
                       dispatch(followButtonPress({ followerId: item._id }));
                     }}
                   >
@@ -131,8 +136,9 @@ export default function User() {
                 (followingobj) => followingobj._id === item._id
               ) && (
                 <button
-                className="text-black w-10/12 border-solid border border-gray-400"
-                  onClick={() => {
+                className="text-black w-10/12 border-solid border border-gray-400 text-sm"
+                  onClick={(e) => {
+                    e.preventDefault()
                     dispatch(followingButtonPress({ followingId: item._id }));
                   }}
                 >
@@ -140,93 +146,22 @@ export default function User() {
                 </button>
               )}
             </div>
+            </Link>
           );
         })}
         </div>
     
-      <div className="px-2 pt-4 pb-12 flex flex-col h-full rounded-t-lg shadow-inner bg-yellow-50 mt-7 ">
-      {userposts !== null && (
-        <div>
+      
+      {userposts.length>0 && (
+        <div className="px-2 pt-4 pb-12 flex flex-col h-full rounded-t-lg shadow-inner bg-yellow-50 mt-7 ">
           {userposts.map((eachpost) => {
             return (
-              <div className="relative px-2 py-2 my-3 rounded-lg shadow-md bg-white">
-              <div className="flex items-center">
-                <img className=" inline w-8 h-8 border-solid border-gray-300 rounded-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
-                <span className="p-1 ">{`${eachpost.user.userName}`}</span>
-              </div>
-              <div className="py-2">{eachpost.caption}</div>
-              <div className="absolute right-1 top-1 text-xs text-gray-300 font-normal">
-                <TimeAgo date={eachpost.date} />
-              </div>
-              <div className="flex w-full">
-              <button
-                className="flex items-center"
-                onClick={() =>
-                  dispatch(
-                    likeButtonPressed({ postID: eachpost._id, userName: userName })
-                  )
-                }
-              >
-                <span class="material-icons-outlined">
-                favorite
-                </span>
-              </button>
-              <span
-              className="text-sm px-1"
-                onClick={() => {
-                  setLikeModal(eachpost._id);
-                  dispatch(likeDataOnTextPressed(eachpost._id));
-                }}
-              >
-                {eachpost.likes.length} Likes
-              </span>
-              <button
-                className=" px-2 flex items-center"
-                onClick={() => {
-                  dispatch(commentButtonPressed(eachpost._id));
-                  setCommentModal(eachpost._id);
-                }}
-              >
-                <span class="material-icons-outlined text-blue-300">
-                 textsms
-                </span>
-              </button>
-              {eachpost.user.userID === userId && (
-                <button
-                  className="ml-auto flex items-center"
-                  onClick={() => {
-                    dispatch(postDeleteButtonPressed({ postdeleteobj: eachpost }));
-                  }}
-                >
-                  <span class="material-icons-outlined">
-                  delete
-                  </span>
-                </button>
-              )}
-              </div>  
-              
-              {commentModal === eachpost._id && (
-                <Comment
-                  postID={eachpost._id}
-                  setCommentModal={setCommentModal}
-                  postObj={eachpost}
-                />
-              )}
-              {likeModal === eachpost._id && (
-                <Like
-                  postID={eachpost._id}
-                  setLikeModal={setLikeModal}
-                  postObj={eachpost}
-                />
-              )}
-
-              
-            </div>
+              <PostComponent eachpost={eachpost}/>
             );
           })}
         </div>
       )}
-      </div>
+      
     </div>
   );
 }
