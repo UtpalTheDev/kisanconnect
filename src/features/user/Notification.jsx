9ceae8404f268330371441924ed49574b5c2516e
Utpal Pati <utpalpati8@gmail.com>
2021-06-28 08:40:56 +0000

    refactor: style add

--- src/features/user/Notification.jsx
+++ src/features/user/Notification.jsx
@@ -4,6 +4,7 @@ import {
   followRequestDataOnNotificationPageLoad,
   followRequestConfirmButtonPress
 } from "./userSlice";
+import TimeAgo from "react-timeago";
 import { useSelector, useDispatch } from "react-redux";
 export default function Notification() {
   let { notification, notificationError } = useSelector((state) => state.user);
@@ -19,9 +20,13 @@ export default function Notification() {
       {followrequestGot !== null &&
         followrequestGot.map((eachrequest) => {
           return (
-            <div className="shadow-md py-2 px-2 bg-white">
-              {eachrequest.userName}
+            <div className="shadow-md py-2 px-2 bg-white flex justify-between items-center">
+              <div className="flex items-center">
+                <img src="https://images.unsplash.com/photo-1520451644838-906a72aa7c86?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGVvcGxlfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="h-10 w-10 rounded-full" />
+                <div className="px-2">{eachrequest.userName}</div>
+                </div>
               <button
+                className="bg-blue-800 py-0.5 px-1 text-white text-sm rounded-md"
                 onClick={() => {
                   dispatch(
                     followRequestConfirmButtonPress({
