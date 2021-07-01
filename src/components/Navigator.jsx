54679892ff81566d9e759403b23532791b2dd0df
Utpal Pati <utpalpati8@gmail.com>
2021-07-01 13:56:12 +0000

    refactor: set style for breakpoints

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -3,7 +3,7 @@ import {BiUser,BiHome,BiBell,BiSearch} from "react-icons/bi"
 export default function Navigator(){
 
     return(
-        <div className="fixed bottom-0 bg-white w-full flex z-10 h-8 justify-between px-3 items-center rounded-t-xl py-6 border border-gray-100">
+        <div className="sm:hidden fixed bottom-0 bg-white w-full flex z-10 h-8 justify-between px-3 items-center rounded-t-xl py-6 border border-gray-100">
         <Link to="/user">
             <BiUser className="text-2xl"/>
         </Link>
