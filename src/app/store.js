3e41f5a6de82752a16a657ece72b4eff8335a4fe
Utpal Pati <utpalpati8@gmail.com>
2021-07-01 15:48:45 +0000

    refactor: news slice add

--- src/app/store.js
+++ src/app/store.js
@@ -4,11 +4,14 @@ import postReducer from "../features/posts/postslice";
 import authReducer from "../features/auth/authSlice";
 import userReducer from "../features/user/userSlice";
 import searchReducer from "../features/search/searchSlice";
+import newsReducer from "../features/news/newsSlice";
+
 export default configureStore({
   reducer: {
     post: postReducer,
     auth: authReducer,
     user: userReducer,
-    search: searchReducer
+    search: searchReducer,
+    news: newsReducer
   }
 });
---
+++ src/features/news/newsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+export const NewsOnLoad = createAsyncThunk(
+    "news/NewsOnLoad",
+    async (errormsg,{ rejectWithValue }) => {
+      try {
+        //console.log("search call");
+        
+          let response = await axios.get(
+            `https://api.thenewsapi.com/v1/news/top?api_token=uWjx9wSBfkieTIEGDCwoICBVfmftqYzil6vhn118&locale=us&limit=4`
+          );
+          return response.data.data;
+        
+        }
+       catch (error) {
+        return rejectWithValue(errormsg);
+      }
+    }
+  );
+
+  export const newsSlice = createSlice({
+      name:'news',
+      initialState:{
+          newsData:[],
+          newsStatus:"idle",
+          newsError:null
+      },
+      reducers:{},
+      extraReducers:{
+          [NewsOnLoad.pending]:(state)=>{
+              state.newsStatus="loading"
+          },
+          [NewsOnLoad.fulfilled]:(state,action)=>{
+            state.newsStatus="succeeded"
+            state.newsData=action.payload
+        },
+        [NewsOnLoad.rejected]:(state,action)=>{
+            state.newsStatus="failed"
+            state.newsError=action.payload
+        },
+      }
+  })
+
+  export default newsSlice.reducer;
