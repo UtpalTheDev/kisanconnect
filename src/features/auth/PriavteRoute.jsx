8e45aec359eae8195a25dfc97c343c97929a7609
Utpal Pati <utpalpati8@gmail.com>
2021-07-20 19:22:22 +0000

    refactor: pathname change

--- src/features/auth/PriavteRoute.jsx
+++ src/features/auth/PriavteRoute.jsx
@@ -1,11 +1,12 @@
-import { Navigate, Link, Route } from "react-router-dom";
+import { Navigate, Link, Route,useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function PrivateRoute({ path, ...props }) {
   const { isUserLogIn } = useSelector((state) => state.auth);
+  let location=useLocation()
   return isUserLogIn ? (
     <Route {...props} path={path} />
   ) : (
-    <Navigate state={{ from: path }} replace to="/login" />
+    <Navigate state={{ from: location.pathname }} replace to="/login" />
   );
 }
